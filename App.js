5955c9566ed1cbfede52f41525dcecf952aa4b8c
Emil Laudon <emil.laudn@gmail.com>
2021-01-05 12:58:08 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button, TextInput, Image } from "react-native";
 
 export default function App() {
+  const image =
+    "https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/thumbs-up.png";
+
+  const buttonPressed = () => console.log("PRESSED THE BUTTON");
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.top} />
+      <View style={styles.headlineBox}>
+        <Text style={styles.headline}>Hello World!</Text>
+        <Text style={{ fontSize: 24, fontWeight: "500" }}>
+          First React Native App
+        </Text>
+        <Text style={{ fontSize: 14 }}>
+          This is a paragraph explaining what this app is all about. As you can
+          see there are already many components including text, images and
+          buttons.
+        </Text>
+      </View>
+
+      <View style={styles.inputBox}>
+        <Text style={{ width: "90%", fontSize: 22, fontWeight: "bold" }}>
+          What would you like to get out of this course?
+        </Text>
+        <TextInput
+          style={{
+            height: "40%",
+            backgroundColor: "#F2F2F2",
+            textAlignVertical: "top",
+          }}
+          placeholder="Initial Text"
+          multiline={true}
+        />
+      </View>
+
+      <View style={styles.emojiBox}>
+        <Text style={{ fontSize: 22, fontWeight: "bold" }}>
+          An emoji describing how you are feeling about the course:
+        </Text>
+        <Image source={{ uri: image }} style={{ height: 100, width: 100 }} />
+      </View>
+
+      <Button title="Submit" onPress={buttonPressed} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    height: "100%",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "white",
   },
+  top: {
+    backgroundColor: "#24305E",
+    width: "100%",
+    height: "5%",
+    justifyContent: "center",
+  },
+  headlineBox: {
+    backgroundColor: "lightblue",
+    width: "100%",
+    height: "18%",
+    justifyContent: "center",
+  },
+  headline: { 
+    fontSize: 30, 
+    fontWeight: "bold" },
+  inputBox: { 
+    height: "25%", 
+    width: "70%", 
+    justifyContent: "center" },
+  emojiBox: { 
+    height: "17%", 
+    width: "70%", 
+    justifyContent: "center" },
 });
